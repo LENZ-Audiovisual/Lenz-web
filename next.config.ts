ba321e7d26e3dd0b7e596b5f73c96dcc5df66c67
Eduardo Braz de Sá <eduardobraz@MacBook-Pro-de-Eduardo.local>
2025-12-25 21:49:15 -0300

    Adicionando netlify.toml para forcar pasta out

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    unoptimized: true, // Necessário para sites estáticos
+    unoptimized: true,
   },
 };
 
